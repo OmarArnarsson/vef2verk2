1850c9e9391b0b0fe67ce573bf80b430448749c1
dezimba <omararnars@gmail.com>
2019-02-05 14:35:21 +0000

    Gagnagrunnur klár

--- schema.sql
+++ schema.sql
@@ -1,3 +1,11 @@
 CREATE TABLE applications (
-  -- TODO schema fyrir töflu
+    id serial primary key,
+    Nafn varchar(64) not null,
+    Netfang varchar(64) not null,
+    Sími  int,
+    Texti varchar(1000),
+    Starf varchar(16),
+    processed boolean default false,
+    created timestamp with time zone not null default current_timestamp,
+    updated timestamp with time zone not null default current_timestamp
 );
